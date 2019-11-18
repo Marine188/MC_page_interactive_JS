3d1487cbf3c33e7cbd2ec3548b939af164577fe9
marine.calderon <marine.calderon@gmail.com>
2019-11-18 17:01:56 +0100

    MAJ

--- script.js
+++ script.js
@@ -11,21 +11,30 @@
 let element = document.getElementsByTagName('footer')[0]; //lance la recherche sur un élément spécifique dans l'arbre DOM.
 
 let count = 1;
+
 if (element.addEventListener("click",function() { //mise en place du compteur
   console.log('Fonctionnalité 1-bis');
   console.log('CLIQUE n°'+`${count}`); // rajoute le nombre de clique avec count
+
   count++;
-
-
 }));
 
 // Fonctionnalité 2 :
+let btn = document.getElementsByClassName("navbar-toggler")[0]; //Sélection du bouton avex l'index
 
-var btn = document.getElementsByClassName("navbar-toggler")[0]; //Sélection du bouton avex l'index
-var navHead = document.getElementById("navbarHeader");
+let navHead = document.getElementById("navbarHeader");
 
 btn.addEventListener('click',function() {
 	console.log("function 2 :" +" " + `${navHead.classList.toggle("collapse")}`); //Affiche sur le site et en console l'état de collapse, true or false
 }); // La méthode toggleAttribute () de l'interface Element bascule un attribut booléen (en le supprimant s'il est présent et en l'ajoutant s'il n'est pas présent) sur l'élément donné.
 
+
 // Fonctionnalité 3 :
+let firstCard = document.getElementsByClassName("card")[0]; //Sélection de la première carte
+let firstEditBtn = document.getElementsByClassName("btn")[3]; //sélection du premier bouton
+
+firstEditBtn.addEventListener("click",function(){
+	firstCard.style = "color:red"; //Passe le texte en rouge
+});
+
+// Fonctionnalité 4 :
